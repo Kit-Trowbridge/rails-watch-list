aabd0f8c93dbbc64bc8b1b7fdad4f5f93fa1cc84
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-09 10:30:42 -0500

    created validations for models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  # A movie has many bookmarks
+  # You can’t delete a movie if it is referenced in at least one bookmark.
+  has_many :bookmarks
+
+  # A movie must have a unique title and an overview.
+  validates :title, :overview, presence: true, uniqueness: true
 end

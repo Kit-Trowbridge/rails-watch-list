083f8339910a17bc258408c89cc4e96a4cf65204
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-09 13:55:30 -0500

    changed bookmark validation so user doesn't have to leave a comment (but if they do, minimum still in place)

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -7,5 +7,6 @@ class Bookmark < ApplicationRecord
   # A bookmark must be linked to a movie and a list, and the [movie, list] pairings should be unique.
   validates :movie_id, uniqueness: { scope: :list_id }
   # The comment of a bookmark cannot be shorter than 6 characters.
-  validates :comment, length: { minimum: 6 }
+  # IF the user is adding a comment
+  validates :comment, length: { minimum: 6 }, allow_blank: true
 end

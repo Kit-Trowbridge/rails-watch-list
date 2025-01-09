175d518d8e967b6e99d3c2bd70cde979a6fbcff8
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-09 12:19:25 -0500

    added bookmarks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
 
 # A user can see all the lists

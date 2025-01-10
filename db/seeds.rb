5648bfeb86356ddf72647e861eafaca8e23b1f5b
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-09 21:10:17 -0500

    made edits to seed file to use api, but haven't seeded yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,35 @@
-require 'faker'
+# require 'faker'
 
-puts 'Cleaning database...'
-Movie.destroy_all
+# puts 'Cleaning database...'
+# Movie.destroy_all
 
-puts 'Creating movies...'
-20.times do
+# puts 'Creating movies...'
+# 20.times do
+#   Movie.create(
+#     title: Faker::Movie.title,
+#     overview: Faker::Movie.quote,
+#     poster_url: Faker::LoremFlickr.image,
+#     rating: rand(1.0..10.0)
+#   )
+# end
+# puts 'Finished'
+
+require 'json'
+require 'open-uri'
+
+# tested in rails console
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+top_films_serialized = URI.parse(url).read
+top_films = JSON.parse(top_films_serialized)['results']
+
+# puts 'Cleaning database...'
+# Movie.destroy_all
+top_films.each do |film|
   Movie.create(
-    title: Faker::Movie.title,
-    overview: Faker::Movie.quote,
-    poster_url: Faker::LoremFlickr.image,
-    rating: rand(1.0..10.0)
+    title: film['title'],
+    overview: film['overview'],
+    poster_url: "http://image.tmdb.org/t/p/original#{film['poster_path']}",
+    rating: film['vote_average']
   )
 end
-puts 'Finished'
